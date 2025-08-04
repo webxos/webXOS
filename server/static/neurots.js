aa7c53d437d4d847f4076ac79e3a20fb5e846500
webXOS <eefeft@gmail.com>
2025-08-04 19:50:55 -0400

    Delete server/static/neurots.js

--- server/static/neurots.js
+++
@@ -1,9 +0,0 @@
-// Placeholder for neural dot visualization
-// Assumed to define initNeurots and setAgentsActive functions
-function initNeurots() {
-    console.log('Initializing neural dots for MCP terminal');
-}
-
-function setAgentsActive(active, agents = [], dna = false, galaxy = false) {
-    console.log(`Setting agents active: ${active}, Agents: ${agents.join(', ')}, DNA: ${dna}, Galaxy: ${galaxy}`);
-}
