c55e016c9d0d3c5bafff130d8f78498a1c34da06
webXOS <eefeft@gmail.com>
2025-08-04 19:38:01 -0400

    Create sw.js

---
+++ server/static/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'webxos-server-v1';
+const urlsToCache = [
+  '/server.html',
+  '/static/style.css',
+  '/static/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
