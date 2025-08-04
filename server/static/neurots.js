3e226d158003cf7c5a8d39a0138228d269b170c3
webXOS <eefeft@gmail.com>
2025-08-04 15:37:02 -0400

    Create neurots.js

---
+++ server/static/neurots.js
@@ -0,0 +1,9 @@
+// Placeholder for neural dot visualization
+// Assumed to define initNeurots and setAgentsActive functions
+function initNeurots() {
+    console.log('Initializing neural dots for MCP terminal');
+}
+
+function setAgentsActive(active, agents = [], dna = false, galaxy = false) {
+    console.log(`Setting agents active: ${active}, Agents: ${agents.join(', ')}, DNA: ${dna}, Galaxy: ${galaxy}`);
+}
