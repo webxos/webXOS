9613124a6ca8e9176d58f28bb6b734e3474c2948
webXOS <eefeft@gmail.com>
2025-08-04 23:36:47 -0400

    Update sw.js

--- server/static/sw.js
+++ server/static/sw.js
@@ -1,3 +1,4 @@
+console.log('[ServiceWorker] Loading sw.js');
 self.addEventListener('install', (event) => {
   console.log('[ServiceWorker] Installing');
   event.waitUntil(self.skipWaiting());
@@ -9,6 +10,7 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  console.log(`[ServiceWorker] Fetching ${event.request.url}`);
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
