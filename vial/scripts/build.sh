68063af7c27f0c562c171d6b263b1402346a6e2a
webXOS <eefeft@gmail.com>
2025-08-09 08:50:03 -0400

    Delete vial directory

--- vial/scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-npm install express ws sqlite3 jsonwebtoken google-auth-library
-pip install torch
-wget https://raw.githubusercontent.com/karpathy/nanoGPT/master/model.py -O src/agents/nanoGPT.py
-chmod -R 755 uploads/
-touch database.db errorlog.md
-chmod 644 database.db errorlog.md
-node src/server.js &
--- vial/scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-npm install -g mocha
-mocha tests/unit/*.test.js
-mocha tests/integration/*.test.js
