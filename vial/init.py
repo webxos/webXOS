883a11f6c96dfdfb1935bf3f35aa41b483a755e5
webXOS <eefeft@gmail.com>
2025-08-14 04:34:24 -0400

    Update init.py

--- vial/init.py
+++ vial/init.py
@@ -1,18 +1,8 @@
-# Package initializer for vial module
-from .auth_manager import authenticate_user
-from .client import MCPClient
-from .export_manager import ExportManager
-from .langchain_agent import LangChainAgent
-from .unified_server import app as fastapi_app
-from .vial_manager import VialManager
-from .webxos_wallet import WebXOSWallet
-
-__all__ = [
-    "authenticate_user",
-    "MCPClient",
-    "ExportManager",
-    "LangChainAgent",
-    "fastapi_app",
-    "VialManager",
-    "WebXOSWallet"
-]
+from .mcp_auth_server import MCPAuthServer
+from .mcp_server_auth import MCPAuthHandler
+from .mcp_server_quantum import MCPQuantumHandler
+from .mcp_server_notes import MCPNotesHandler
+from .mcp_server_resources import MCPResourcesHandler
+from .webxos_wallet import MCPWalletManager
+from .auth_manager import AuthManager
+from .quantum_simulator import QuantumSimulator
