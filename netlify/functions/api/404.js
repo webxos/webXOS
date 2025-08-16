8e83abc4f92f908bc7253a551f852f8636f485a5
webXOS <eefeft@gmail.com>
2025-08-16 07:56:06 -0400

    Delete netlify directory

--- netlify/functions/api/404.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 404,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ error: 'Endpoint not found', path: event.path })
-  };
-};
