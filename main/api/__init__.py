c58e3bfa2c2c6048b53b6e0806e9ac8587e19d51
webXOS <eefeft@gmail.com>
2025-08-17 01:23:46 -0400

    Update __init__.py

--- main/api/__init__.py
+++ main/api/__init__.py
@@ -1,1 +1,6 @@
-# Package initialization
+# Package initializer for utils module
+from .logging import log_info, log_error, notify_message
+from .monitoring import monitor, start_monitoring
+from .authentication import verify_token
+from .rag import SmartRAG
+from .md_processor import MongoDBMarkdownProcessor
