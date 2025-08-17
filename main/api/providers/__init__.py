ede19d5d835184a248cea47092b616f49c0c538b
webXOS <eefeft@gmail.com>
2025-08-17 01:46:52 -0400

    Create __init__.py

---
+++ main/api/providers/__init__.py
@@ -0,0 +1,5 @@
+# Package initializer for providers module
+from .anthropic_provider import router as anthropic_router
+from .openai_provider import router as openai_router
+from .xai_provider import router as xai_router
+from .google_provider import router as google_router
