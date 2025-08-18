1f58d802234a7656dee3dc8622634fed63f86a8a
webXOS <eefeft@gmail.com>
2025-08-18 19:57:27 -0400

    Create __init__.py

---
+++ vial2/mcp/__init__.py
@@ -0,0 +1,7 @@
+from .api.api_router import api_router
+from .config.config_manager import config
+from .error_logging.error_log import log_error
+
+__all__ = ["api_router", "config", "log_error"]
+
+# xAI Artifact Tags: #vial2 #mcp #init #module #neon_mcp
