884717396c0bca1d9a0612d7a033e4033a93cd59
webXOS <eefeft@gmail.com>
2025-08-18 15:01:18 -0400

    Update __init__.py

--- vial2/__init__.py
+++ vial2/__init__.py
@@ -1,3 +1,31 @@
-# __init__.py for vial2/
-# Vial MCP v2 backend
-# xAI Artifact Tags: #vial2 #backend #neon_mcp
+from .vial_manager import router as vial_manager_router
+from .vial_status import router as vial_status_router
+from .vial_config import vial_config
+from .vial_migrations import run_migrations
+from .vial_proof_of_work import router as pow_router
+from .vial_sync import router as sync_router
+from .vial_wallet_bridge import router as wallet_bridge_router
+from .vial_metrics import router as metrics_router
+from .vial_auth import router as auth_router
+from .vial_task_queue import router as task_queue_router
+from .vial_export import router as export_router
+from .vial2_pytorch_controller import router as pytorch_router
+from .vial2_offline import router as offline_router
+
+__all__ = [
+    "vial_manager_router",
+    "vial_status_router",
+    "vial_config",
+    "run_migrations",
+    "pow_router",
+    "sync_router",
+    "wallet_bridge_router",
+    "metrics_router",
+    "auth_router",
+    "task_queue_router",
+    "export_router",
+    "pytorch_router",
+    "offline_router"
+]
+
+# xAI Artifact Tags: #vial2 #init #pytorch #sqlite #octokit #neon_mcp
